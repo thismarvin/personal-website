02d14bb11e99e880d0a038ea90d3feb340ecf40e
thismarvin <bend_@outlook.com>
2019-10-23 19:22:57 -0500

    potentially fixed speed up problem

--- modules/Wave.mjs
+++ modules/Wave.mjs
@@ -47,7 +47,7 @@ export default class Wave {
                     //y = p5.height * 0.70 + noiseValue - instance.yOffset;
 
                     noiseValue = p5.noise(p5.map(x, 0, p5.width + 5, 0, noiseWidth) + noiseOffset);
-                    noiseValue = p5.sin(p5.map(x, 0, p5.width + 5, 0, p5.PI / 2) + waveOffset * noiseValue + noiseValue);                    
+                    noiseValue = p5.sin(p5.map(x, 0, p5.width + 5, 0, p5.PI / 2) + waveOffset + noiseValue);                    
                     noiseValue *= p5.height * 0.10;
                     y = p5.height * 0.80 + noiseValue - instance.yOffset;
 
