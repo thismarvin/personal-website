3a7281f5e35a35318b6127e58d80e305a5aabb9a
thismarvin <bend_@outlook.com>
2019-10-25 15:31:33 -0500

    stay wavy 🌊🌊🌊

--- javascript/main.mjs
+++ javascript/main.mjs
@@ -1,3 +1,9 @@
 import Wave from "../javascript/modules/Wave.mjs";
 
-const wave = new Wave("wave");
+const waves = [
+    new Wave("wave-landing", 0.8, 0.1, "#000000"),
+    new Wave("wave-one", 0.6, 0.30, "#FF004D"),
+    new Wave("wave-two", 0.6, 0.30, "#000000"),
+    new Wave("wave-three", 0.6, 0.30, "#BD00FF"),
+    new Wave("wave-four", 0.6, 0.30, "#000000"),
+];
