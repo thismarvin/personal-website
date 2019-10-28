fe0df2ea8493135f74953d3995b192385d77f61e
thismarvin <bend_@outlook.com>
2019-10-28 02:23:22 -0500

    complete style rework

--- javascript/main.mjs
+++ javascript/main.mjs
@@ -1,9 +1,9 @@
 import Wave from "../javascript/modules/Wave.mjs";
 
 const waves = [
-    new Wave("wave-landing", 0.8, 0.1, "#000000"),
+    new Wave("wave-landing", 0.6, 0.3, "#000000"),
     new Wave("wave-one", 0.6, 0.30, "#FF004D"),
     new Wave("wave-two", 0.6, 0.30, "#000000"),
-    new Wave("wave-three", 0.6, 0.30, "#BD00FF"),
+    new Wave("wave-three", 0.6, 0.30, "#FF004D"),
     new Wave("wave-four", 0.6, 0.30, "#000000"),
 ];
