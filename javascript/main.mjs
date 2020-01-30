38ac2cae3e03b0469102968f112e4c211505d4c0
thismarvin <bend_@outlook.com>
2020-01-30 12:54:41 -0600

    setup basic vue project

--- javascript/main.mjs
+++
@@ -1,9 +0,0 @@
-import Wave from "../javascript/modules/Wave.mjs";
-
-const waves = [
-    new Wave("wave-landing", 0.5, 0.3, "#000000"),
-    new Wave("wave-one", 0.5, 0.3, "#FF004D"),
-    new Wave("wave-two", 0.5, 0.3, "#000000"),
-    new Wave("wave-three", 0.5, 0.3, "#FF004D"),
-    new Wave("wave-four", 0.5, 0.3, "#000000"),
-];
