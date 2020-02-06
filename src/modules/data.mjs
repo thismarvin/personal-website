b52173b3295a1eb251c2bdca1d8916fa3863b25f
thismarvin <bend_@outlook.com>
2020-02-05 20:17:13 -0600

    touched up collection data

--- src/modules/data.mjs
+++ src/modules/data.mjs
@@ -38,7 +38,7 @@ collectionGameJams.addEntry({
     title: "Robosses",
     subtitle: "boss rush platformer",
     image: "robosses.gif",
-    description: "Robosses is a 2D Boss Rush Platformer written in Python using the Pygame library for the GameShell Game Jam (2019Q4).",
+    description: "A 2D Boss Rush Platformer written in Python using the Pygame library for the GameShell Game Jam (2019Q4).",
     background: "The year is 3005 and the human race has been enslaved by an animal-like robotic race. The resistance has built YOU (also a robot) to fight against the invaders. This is your story.",
     sourceCode: "https://github.com/thismarvin/robosses",
     projectPage: "https://team-am-squared.itch.io/robosses",
@@ -47,8 +47,8 @@ collectionGameJams.addEntry({
 collectionGameJams.addEntry({
     title: "Quantum Caverns",
     subtitle: "action platformer",
-    image: "pit.gif",
-    description: "Quantum Caverns is a 2D Platformer written in Python using the Pygame library for the GameShell Game Jam (2019Q3).",
+    image: "quantum_caverns.gif",
+    description: "A 2D Platformer written in Python using the Pygame library for the GameShell Game Jam (2019Q3).",
     background: "Explore a mysterious cavern while you shift through dimensions on your way to victory. Keep a look out for any pesky quantum crabs!",
     sourceCode: "https://github.com/thismarvin/quantum-caverns",
     projectPage: "https://team-am-squared.itch.io/quantum-caverns",
@@ -57,8 +57,8 @@ collectionGameJams.addEntry({
 collectionGameJams.addEntry({
     title: "Diner Mafia",
     subtitle: "role playing game",
-    image: "town.gif",
-    description: "Diner Mafia is a 2D RPG written in Python using the Pygame library for the GameShell Game Jam (2019Q2).",
+    image: "diner_mafia.gif",
+    description: "A 2D RPG written in Python using the Pygame library for the GameShell Game Jam (2019Q2).",
     background: "It's the 1920s and you are a mafia member running a black market of diner goods. Embark on a journey through the seas to collect coveted coffee beans, or go fishing at the local beach to collect various fish.",
     sourceCode: "https://github.com/austin0209/diner-mafia",
     projectPage: "https://team-am-squared.itch.io/diner-mafia",
@@ -69,7 +69,7 @@ export const collectionCSharp = new ProjectCollection("csharp");
 collectionCSharp.setInformation({
     technologies: "C# + MonoGame",
     header: "C# Projects",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis odio quibusdam aspernatur excepturi voluptates voluptatum praesentium dolore, quo ut exercitationem, asperiores iure quia at officiis repellat tempore sunt nostrum enim.",
+    description: "Throughout the years I have created hundreds of prototypes using various C# libraries and frameworks. With the integration of the MonoGame Framework, I have created some projects worth checking out.",
     waveTop: "#000000",
     waveBottom: "#242424"
 });
@@ -78,17 +78,27 @@ collectionCSharp.addEntry({
     title: "Morro",
     subtitle: "game engine",
     image: "me_preview.png",
-    description: "Morro is a cross platform 2D game engine written in C# on top of the MonoGame Framework.",
+    description: "An open-source cross platform 2D game engine written in C# on top of the MonoGame Framework.",
     background: "Morro is a passion project that is constantly being tweaked and improved. Morro mainly caters to 2D games, but it also can be used for any sort of creative visualizations.",
     sourceCode: "https://github.com/thismarvin/morro",
     projectPage: ""
 });
 
 collectionCSharp.addEntry({
+    title: "Racing Network",
+    subtitle: "machine learning",
+    image: "racing_network.gif",
+    description: "A machine learning project that uses an evolutionary algorithm to get car agents around a user defined race track.",
+    background: "???",
+    sourceCode: "https://github.com/thismarvin/racing-network",
+    projectPage: ""
+});
+
+collectionCSharp.addEntry({
     title: "Larry the Lightbulb",
     subtitle: "action platformer",
-    image: "spin.gif",
-    description: "Larry the Lightbulb is a 2D Platformer written in C# using the MonoGame Framework.",
+    image: "larry_the_lightbulb.gif",
+    description: "A 2D Platformer written in C# using the MonoGame Framework that is available on PC and mobile.",
     background: "Larry the Lightbulb runs natively on Windows, and will run on MacOS and Linux if the Mono Framework is installed. Larry the Lightbulb is also available for free on iOS and Android!",
     sourceCode: "",
     projectPage: "https://flourflo.itch.io/larry-the-lightbulb"
